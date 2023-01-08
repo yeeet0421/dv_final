a5bed97fa8246f63f9f850c7c6137a7398be80cb
cindy203cc <cindy203cc@gmail.com>
2023-01-09 05:50:25 +0800

    sentiment analysis done

--- data/vid_sentiment.py
+++ data/vid_sentiment.py
@@ -1,14 +1,14 @@
 import json
 fileName = '烏克蘭'
-with open(fileName + '_comment_sentiment.json', encoding='utf8') as jsonfile:
-    comment_data = json.load(jsonfile)
-with open(fileName + '_vid.json', encoding='utf8') as jsonfile:
-    vid_data = json.load(jsonfile)
+with open(fileName + '_comment_sentiment.json', encoding='utf8') as jsonfile1:
+    comment_data = json.load(jsonfile1)
+with open(fileName + '_vid.json', encoding='utf8') as jsonfile2:
+    vid_data = json.load(jsonfile2)
 
 for video in comment_data:
     sentiments = []
     for id, comment in enumerate(comment_data[video]):
-        if 'avg_sentiment' in sentiments:
+        if 'avg_sentiment' in comment:
             sentiments.append(comment['avg_sentiment'])
 
     # discard no comments video
@@ -17,6 +17,6 @@ for video in comment_data:
         continue
 
     vid_data[video]['avg_sentiment'] = sum(sentiments) / len(sentiments)
-        
+
 with open(fileName + '_vid_sentiment.json', 'w', encoding='utf8') as output:
     json.dump(vid_data, output, indent=4, ensure_ascii=False)
