009d36335a153aa3500f34ed66ac252b426eb7c7
yeeet0421 <nctucs.cs08@nycu.edu.tw>
2023-01-10 00:14:33 +0800

    channel data thing

--- data/video_scrape.py
+++ data/video_scrape.py
@@ -138,6 +138,57 @@ def download_comments(youtube, vid):
         return []
         #videos[i].update({'comment_exist':'F', 'comment_count':0})
 
+def channel_playlist(youtube, id):
+    request = youtube.channels().list(
+        part="id, contentDetails",
+        id=id
+    )
+    response = request.execute()
+    return response
+
+def get_plays_each(youtube, nextPageToken, id, comments):
+    # for video in vlist:
+    #     vid = video['videoid']
+    #     #print("vid = ",vid)
+    try:
+        results = youtube.playlistItems().list(
+            part="id, snippet",
+            maxResults=50,
+            playlistId=id
+        ).execute()
+        while results: 
+            for item in results["items"]:
+                if 'snippet' in item:
+                    # print(data)
+                    comments.append(item['snippet']['resourceId']['videoId'])
+            if 'nextPageToken' in results:
+                results = youtube.playlistItems().list(
+                    part="id, snippet",
+                    maxResults=50,
+                    playlistId=id,
+                    pageToken = results['nextPageToken']
+                ).execute()
+            else:
+                break
+        # extracting required info
+        # from each result object 
+        # return results
+    except Exception as e: print(e)
+        # print("videoid: ",vid," do not have comments!")
+    return comments
+
+def play_vidlist(youtube, id):
+    print(id)
+    vids = []
+    vids = get_plays_each(youtube, "", id, vids)
+    if vids:
+        #videos_3 = pd.DataFrame(videos_3)
+        print('length: ', len(vids))
+        return vids
+        #videos[i].update({'comment_exist':'T', 'comment_count':len(comments)})
+    else:
+        print("playid: ",id," do not allow/have vids!")
+        return []
 
 def main(youtube, keyword, method):
 
@@ -147,7 +198,6 @@ def main(youtube, keyword, method):
         vids = list(data.keys())
         vids_info = {}
         for vid in vids:
-
             vids_info[vid] = (get_vids_info(youtube, vid))
         with open(keyword+'_vid.json', 'w', encoding='utf-8') as f:
             json.dump(vids_info, f, ensure_ascii=False, indent=4)
@@ -165,13 +215,40 @@ def main(youtube, keyword, method):
                 coms_dict[vid] = comments
         with open(keyword+'_comment.json', 'w', encoding='utf-8') as f:
             json.dump(coms_dict, f, ensure_ascii=False, indent=4)
-       
+
+    elif method == 'channel':
+        f = open(keyword+'_vid.json')
+        data = json.load(f)
+        chan = []
+        for vid in data:
+            chan.append(data[vid]['items'][0]['snippet']['channelId'])
+        chan_list = list(set(chan))
+        chan_info = {}
+        for chan in chan_list:
+            chan_info[chan] = channel_playlist(youtube, chan)
+        with open(keyword+'_chanPlay.json', 'w', encoding='utf-8') as f:
+            json.dump(chan_info, f, ensure_ascii=False, indent=4)
+        # print(":))")
+
+    elif method == 'play':
+        f = open(keyword+'_chanPlay.json')
+        data = json.load(f)
+        plays = []
+        for chan in data:
+            plays.append(data[chan]['items'][0]['contentDetails']['relatedPlaylists']['uploads'])
+        print(len(plays))
+        play_info = {}
+        for play in plays:
+            play_info[play] = play_vidlist(youtube, play)
+        with open(keyword+'_playVid.json', 'w', encoding='utf-8') as f:
+            json.dump(play_info, f, ensure_ascii=False, indent=4)
         # print(":))")
     
     f.close()
 
 if __name__ == "__main__":
     youtube = init()
+    # youtube = 1
     # main(youtube, '1922', 'comment')
     for keyword in ['1922', '世足賽', '地震', '疫情', '烏克蘭']:
-        main(youtube, keyword, 'comment')
+        main(youtube, keyword, 'play')
