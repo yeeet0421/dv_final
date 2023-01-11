aa622d8aa94285ec5c0d9369b934bef2255a0290
pete0707 <lin860707.cs10@nycu.edu.tw>
2023-01-11 19:03:53 +0800

    fix overflowing words which were cut

--- viz_final/src/MyWordCloud.js
+++ viz_final/src/MyWordCloud.js
@@ -1,10 +1,10 @@
 import React , { useCallback, useState } from 'react';
-import { render } from 'react-dom';
+//import { render } from 'react-dom';
 import WordCloud from 'react-d3-cloud';
 import { scaleOrdinal } from 'd3-scale';
 import { schemeCategory10 } from 'd3-scale-chromatic';
-
-
+import { CSSTransition } from 'react-transition-group';
+import './WordCloud.css'
 //import words from "./word.js";
 
 const schemeCategory10ScaleOrdinal = scaleOrdinal(schemeCategory10);
@@ -21,35 +21,50 @@ const MyWordCloud = ({height, width, data, setKeyword, setUIProgress, setVideoID
     const fontSize = useCallback((word) => Math.log2(word.value) * 5, []);
     const rotate = useCallback(() => 0, []);
     const fill = useCallback((d, i) => schemeCategory10ScaleOrdinal(i), []);
-    
+    /*
+    const styles = {
+        height: width*(7/6),
+        width: width
+    }
+    */
+    const styles = {
+        height: height,
+        width: width,
+        alignItems: "center",
+    justifyContent: "center",
+    }
+      
 
 
     return(
-        <div style={{height: height, width: width}}>
-            <WordCloud
-                data={data}
-                //width={width}
-                //height={height}
-                font="Times"
-                fontStyle="italic"
-                fontWeight="bold"
-                fontSize={fontSize}
-                spiral="rectangular"
-                rotate={rotate}
-                padding={5}
-                fill={fill}
-                onWordClick={onWordClick}
-                setUIProgress = {setUIProgress}
-                onWordMouseOver={(event, d) => {
-                    console.log(`onWordMouseOver: ${d.text}`);
-                    console.log(d);
-                    d.cursor='pointer';
-                }}
-                /*
-                onWordMouseOut={(event, d) => {
-                    console.log(`onWordMouseOut: ${d.text}`);
-                }}*/
-        />
+        <div style={styles} className = "parent-div">
+            <CSSTransition in={true} timeout={1000} classNames="fade">
+                <WordCloud
+                    data={data}
+                    //width={width}
+                    //height={height}
+                    font="Arial"
+                    fontStyle="italic"
+                    fontWeight="bold"
+                    fontSize={fontSize}
+                    spiral="rectangular"
+                    rotate={rotate}
+                    padding={2}
+                    fill={fill}
+                    onWordClick={onWordClick}
+                    //setUIProgress = {setUIProgress}
+                    /*onWordMouseOver={(event, d) => {
+                        console.log(`onWordMouseOver: ${d.text}`);
+                        console.log(d);
+                        d.cursor='pointer';
+                    }}*/
+                    /*
+                    onWordMouseOut={(event, d) => {
+                        console.log(`onWordMouseOut: ${d.text}`);
+                    }}*/
+                />
+            </CSSTransition>
+            
         </div>
     );
     };
--- viz_final/src/WordCloudFrame.js
+++ viz_final/src/WordCloudFrame.js
@@ -13,10 +13,12 @@ const WordCloudFrame = ({windowWidth, windowHeight, data, trends, setUIProgress,
         display: "flex"
       };
       
-      const leftDivStyles = {
-        flex: 1,
-        overflow: "hidden",
-      };
+    const leftDivStyles = {
+    flex: 1,
+    overflow: "hidden",
+    alignItems: "center",
+    justifyContent: "center",
+    };
       
       const rightDivStyles = {
         flex: 1,
